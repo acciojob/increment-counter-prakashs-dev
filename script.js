4cb32789de86fddfaa75064d680ee85adf902dc6
Prakash S <79446554+prakashs-dev@users.noreply.github.com>
2023-11-20 15:01:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,6 @@
 function btnClick() {
   let number = document.getElementById('counter');
   let value = parseInt(number.innerText);
-  alert(`${value}`);
+  alert(value);
   number.innerText = ++value;
 }
